d873af16f84457989efa5b152051a308d0edd732
Tavishi06 <tavishikashyap02@gmail.com>
2026-09-21 08:30:08 +0530

    Array List operations

--- arraylist2.java
+++ arraylist2.java
@@ -9,6 +9,8 @@ public class arraylist2 {
 
          ArrayList<Integer> numbers = new ArrayList<>();
 
+         // Taking input from user and adding to the ArrayList
+
          for(int i=0; i<5; i++){
              System.out.print("Enter no " + (i+1) + ": ");
              int num = sc.nextInt();
@@ -16,5 +18,34 @@ public class arraylist2 {
          }
 
         System.out.println("The numbers you entered are: " + numbers);
+
+        // Calculating the sum of the numbers in the ArrayList
+
+        int sum = 0;
+        
+        for (Integer number : numbers) {
+            sum += number;
+        }
+        
+        System.out.println("Sum = " + sum);
+
+        // Finding the maximum number in the ArrayList
+
+        int max = numbers.get(0);
+        
+        for (Integer number : numbers) {
+            
+            if (number > max) {
+            max = number;
+            }
+        }
+        
+        System.out.println("Maximum = " + max);
+
+        // Removing elements from the ArrayList by two methods: by index and by value
+
+        numbers.remove(1);                                    // by index
+        numbers.remove(Integer.valueOf(30));                      // by value
+        System.out.println("Numbers after removal: " + numbers);
     }
 }
