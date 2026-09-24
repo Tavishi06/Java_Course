e714d5398aaee5ed9833f2250a807ae1431d6bb8
Tavishi06 <tavishikashyap02@gmail.com>
2026-09-24 18:13:42 +0530

    List interface

---
+++ list.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class list {
+
+    public static void main(String[] args) {
+
+        List<Integer> numbers = new ArrayList<>();
+
+        numbers.add(10);
+        numbers.add(20);
+        numbers.add(30);
+
+        System.out.println(numbers);
+    }
+}
